96ad39188b001b6ccd9f2dfacb14d05b541938e4
doobo <doobo@foxmail.com>
2021-06-18 11:50:06 +0800

    去掉不必要的代码

--- elfinder-core/src/main/java/cn/yours/elfinder/configuration/CmdObservedUtils.java
+++ elfinder-core/src/main/java/cn/yours/elfinder/configuration/CmdObservedUtils.java
@@ -3,13 +3,11 @@ package cn.yours.elfinder.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Observable;
-
 /**
- * 被观察者
+ * 被观察者工具类
  */
 @Component
-public class CmdObservedUtils extends Observable {
+public class CmdObservedUtils {
     
     private static CmdObserved INSTANCE;
     
