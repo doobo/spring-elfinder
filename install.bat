e522d2d09a06f68457a85968aee041f7f8cd34b9
doobo <doobo@foxmail.com>
2025-10-11 17:54:56 +0800

    更新发布脚本

--- install.bat
+++ install.bat
@@ -1,16 +1,4 @@
-#!/bin/bash
-#git init .
-#git remote add origin git@github.com:doobo/spring-elfinder.git
-#git add .
-#git commit -m "版本更新"
-#git push origin master --force
-#git branch --set-upstream-to=origin/master master
 
-#git remote add gitee git@gitee.com:doobo/spring-elfinder.git
-#git push gitee master --force
-
-mvn clean
-mvn release:prepare
-mvn release:perform
-
-#mvn clean deploy -P release
+CALL mvn clean
+CALL mvn release:prepare
+CALL mvn release:perform
